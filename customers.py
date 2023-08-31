d903e397e23a10bb75a5b4109870954eaede9076
David <david.ndiba@student.moringaschool.com>
2023-08-31 09:11:21 +0300

    customers.py

---
+++ customers.py
@@ -0,0 +1,46 @@
+class Customer:
+    all_customers = []
+
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
+        Customer.all_customers.append(self)
+
+    def set_given_name(self, given_name):
+        self.given_name = given_name
+
+    def set_family_name(self, family_name):
+        self.family_name = family_name
+
+    def given_name(self):
+        return self.given_name
+
+    def family_name(self):
+        return self.family_name
+
+    def full_name(self):
+        return f"{self.given_name} {self.family_name}"
+
+    @classmethod
+    def all(cls):
+        return cls.all_customers
+
+customer1 = Customer("George", "Odhiambo")
+customer2 = Customer("John", "Maina")
+customer3 = Customer("David", "Ndiba")
+customer4 = Customer("Nahashon", "Waichai")
+customer5 = Customer("Newton", "Jenga")
+
+#update
+customer1.set_given_name("Derrick")
+customer2.set_family_name("Ojongo")
+
+# Get customer information
+print(customer1.given_name())  
+print(customer2.family_name()) 
+print(customer1.full_name())   
+
+# Get all customer instances
+all_customers = Customer.all()
+for customer in all_customers:
+    print(customer.full_name())
